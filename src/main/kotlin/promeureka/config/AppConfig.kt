9eb849513ad9c69c88e5b8680e18c739a749394e
Pedro Rodrigues <hs.pedro.rodrigues@gmail.com>
2018-05-18 18:35:12 -0300

    Simplifying configurations

--- src/main/kotlin/promeureka/config/AppConfig.kt
+++ src/main/kotlin/promeureka/config/AppConfig.kt
@@ -3,23 +3,12 @@ package promeureka.config
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.cloud.context.config.annotation.RefreshScope
 import org.springframework.stereotype.Component
+import java.io.File
 
 @Component
 @RefreshScope
-open class AppConfig {
+open class AppConfig constructor(@Value("\${prom-eureka.json-file.path}") private val filePath: String) {
 
-    @Value("\${prometheus.jsonFolder}")
-    var jsonFolder: String? = null
-
-    fun getAbsoluteFileName(): String {
-        jsonFolder?.let {
-            if (it.endsWith("/") || it.endsWith("\\")) {
-                return it.trimEnd('/').trimEnd('\\')
-            } else {
-                return it
-            }
-        }
-        throw RuntimeException("Error! Invalid prometheus.jsonFolder! Value: " + jsonFolder)
-    }
-
+    fun getAbsoluteFileName(): String =
+            if (filePath.endsWith(File.separatorChar)) filePath.trimEnd(File.separatorChar) else filePath
 }
