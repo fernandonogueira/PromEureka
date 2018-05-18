fd6f667202e9663c1f5023bf14e9e1b0511fb2bc
Pedro Rodrigues <hs.pedro.rodrigues@gmail.com>
2018-05-18 18:35:45 -0300

    Changing some classes to data classes and improving worker

--- src/main/kotlin/promeureka/scheduled/EurekaPrometheusInstanceExposeWorker.kt
+++ src/main/kotlin/promeureka/scheduled/EurekaPrometheusInstanceExposeWorker.kt
@@ -13,100 +13,52 @@ import promeureka.model.PrometheusFileSDItem
 import promeureka.model.PrometheusLabel
 import java.nio.charset.StandardCharsets
 import java.nio.file.Files
-import java.nio.file.Path
 import java.nio.file.Paths
-import java.nio.file.StandardCopyOption
-import java.util.HashMap
-import kotlin.collections.ArrayList
-
 
 @Component
 open class EurekaPrometheusInstanceExposeWorker
-@Autowired constructor(val discoveryClient: DiscoveryClient,
-                       @Qualifier("prettyObjectMapper") val objectMapper: ObjectMapper,
-                       val appConfig: AppConfig) {
-
-    private val log = LoggerFactory.getLogger(EurekaPrometheusInstanceExposeWorker::class.java)
+@Autowired constructor(val client: DiscoveryClient,
+                       @Qualifier("prettyObjectMapper") val mapper: ObjectMapper,
+                       val config: AppConfig) {
 
     @Scheduled(fixedDelay = 60000, initialDelay = 20000)
-    fun saveInstances() {
+    fun run() {
+        val pairs = getInstancesByService(client)
+        val prometheusItems = getPrometheusItems(pairs)
 
-        val serviceInstancesMap = HashMap<String, List<ServiceInstance>>()
-
-        val services = discoveryClient.services
-
-        log.info("Services: {}", services)
-
-        if (services != null && !services.isEmpty()) {
-
-            log.info("{} services found. Retrieving instances...", services.size)
-
-            services.forEach {
-                val serviceInstances = discoveryClient.getInstances(it)
-                if (serviceInstances != null && !serviceInstances.isEmpty()) {
-                    log.info("{} instances found for service: [{}]", serviceInstances.size, it)
-                    serviceInstancesMap.put(it, serviceInstances)
-                } else {
-                    log.info("No instances found for service: [{}]", it)
-                }
-            }
-
-        } else {
-            log.info("No services found.")
-        }
-
-
-        if (!serviceInstancesMap.isEmpty()) {
-            log.info("Completed discovering. Result: {}", serviceInstancesMap)
-
-
-            val prometheusItems = ArrayList<PrometheusFileSDItem>()
-
-            serviceInstancesMap.entries.forEach {
-
-                val item = PrometheusFileSDItem()
-
-                val urlList = ArrayList<String>()
-
-                it.value.forEach {
-                    urlList.add("${it.host}:${it.port}")
-                    log.info("uri [{}], " +
-                            "secure? [{}], " +
-                            "host: [{}], " +
-                            "port: [{}], " +
-                            "serviceId: [{}], " +
-                            "metadata: [{}]", it.uri,
-                            it.isSecure,
-                            it.host,
-                            it.port,
-                            it.serviceId,
-                            it.metadata)
-                }
-
-                item.targets = urlList
-                item.labels = PrometheusLabel(it.key)
-
-                prometheusItems.add(item)
-            }
-
-            if (!prometheusItems.isEmpty()) {
-                log.info("Items parsed... Converting to JSON...")
-                val json = objectMapper.writeValueAsString(prometheusItems)
-                log.info("Converted JSON: {}", json)
-                val createdFilePath: Path = Files.createTempFile("prometheus-eureka-instances", ".json")
-                Files.write(createdFilePath, json.toByteArray(StandardCharsets.UTF_8))
-                log.info("File created. Path: {}", createdFilePath.toFile().absolutePath)
-
-                val destination = "${appConfig.getAbsoluteFileName()}/eureka-instances.json"
-                log.info("Moving file to destination: {}", destination)
-                Files.move(createdFilePath,
-                        Paths.get("${appConfig.getAbsoluteFileName()}/eureka-instances.json"),
-                        StandardCopyOption.REPLACE_EXISTING)
-                log.info("File moved.")
-            }
-
-        }
-
+        persist(prometheusItems)
     }
 
+    private fun getInstancesByService(client: DiscoveryClient) = client
+            .services
+            ?.also { service -> LOGGER.info("Retrieved Services: [{}]", service) }
+            ?.map { service -> Pair(service, client.getInstances(service)) }
+            ?.onEach { pair -> LOGGER.info("[{}] - Retrieved Instances: [{}]", pair.first, pair.second) }
+            ?.filter { pair -> pair.second != null }
+            ?.map { pair -> Pair(pair.first, pair.second!!) }
+
+    private fun getPrometheusItems(pairs: List<Pair<String, List<ServiceInstance>>>?) = pairs
+            ?.map { pair ->
+                PrometheusFileSDItem(
+                        PrometheusLabel(pair.first),
+                        pair.second.map { instance -> "${instance.host}:${instance.port}" }
+                )
+            }
+
+    private fun persist(items: List<PrometheusFileSDItem>?) {
+        val json = mapper.writeValueAsString(items)
+        val destination = Paths.get("${config.getAbsoluteFileName()}/eureka-instances.json")
+
+        LOGGER.info("Converted JSON: {}", json)
+
+        Files.write(destination, json.toByteArray(StandardCharsets.UTF_8))
+
+        LOGGER.info("File created. Path: {}", destination)
+    }
+
+    companion object {
+
+        @JvmStatic
+        private val LOGGER = LoggerFactory.getLogger(EurekaPrometheusInstanceExposeWorker::class.java)
+    }
 }
